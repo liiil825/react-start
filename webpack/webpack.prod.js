84a380449a3d01bacf968a4a44a979a3426bc38b
James Leee <40129878@qq.com>
2017-03-20 21:15:28 +0800

    change name
    Longer explanation of the motivation for the change
    
    Fixes-Bug: Enter bug-id or deleter line
    Implements-Requirement: Enter requirement-id or delete line

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,11 +2,12 @@
 
 module.exports = {
   devtool: false,
-  // recordsPath: 'records.json',
-  // resolve: {
-  //   alias: {
-  //     moment: resolve(__dirname, '../', 'node_modules/moment/min/moment.min.js'),
-  //   },
-  // },
+  recordsPath: 'records.json',
+  resolve: {
+    alias: {
+      // moment: resolve(__dirname, '../', 'node_modules/moment/min/moment.min.js'),
+    },
+    extensions: ['.js', '.jsx'],
+  },
 }
 
